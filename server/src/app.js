6edef7ccef0ec29712c0c2e43b98e7756c2ffa77
Pedro Lucas S. Cabral <63625419+PedroLSCabral@users.noreply.github.com>
2025-04-12 20:09:18 -0300

    Adição de cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const produtoRoutes = require('./routes/produtoRoutes');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
+
+app.use(cors({origin: '*'}));
 
 app.use(bodyparser.json());
 app.use(produtoRoutes);
