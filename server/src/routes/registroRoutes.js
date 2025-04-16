482f08fa453fa5367484df43adc0932e500eb967
Pedro Lucas S. Cabral <63625419+PedroLSCabral@users.noreply.github.com>
2025-04-16 09:38:23 -0300

    Correção de rotas de registro

--- server/src/routes/registroRoutes.js
+++ server/src/routes/registroRoutes.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const registroController = require('../controllers/registroController');
 
-router.post('/clientes', registroController.cadastrarCliente);
+router.post('/registro/clientes', registroController.cadastrarCliente);
 
-router.post('/funcionarios', registroController.cadastrarFuncionario);
+router.post('/registro/funcionarios', registroController.cadastrarFuncionario);
 
-router.post('/produtos', registroController.cadastrarProduto);
+router.post('/registro/produtos', registroController.cadastrarProduto);
 
-router.post('/ingredientes', registroController.cadastrarIngrediente);
+router.post('/registro/ingredientes', registroController.cadastrarIngrediente);
+
+router.post('/registro/produtoingrediente', registroController.cadastrarProdutoIngrediente);
 
 module.exports = router;
