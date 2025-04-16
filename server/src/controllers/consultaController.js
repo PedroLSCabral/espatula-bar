bbc9e02176fed69b4ae1e99231c5bd6c84312113
Pedro Lucas S. Cabral <63625419+PedroLSCabral@users.noreply.github.com>
2025-04-16 16:40:53 -0300

    Criação de rotas e controller de consulta

---
+++ server/src/controllers/consultaController.js
@@ -0,0 +1,71 @@
+const {Produto, Ingrediente, ProdutoIngrediente, Funcionario, Cliente, Venda} = require('../models');
+
+class ConsultaController {
+    async consultarProduto(req, res) {
+        try {
+            const produtos = await Produto.findAll({});
+            res.status(200).json(produtos);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Erro ao consultar produtos' });
+        }
+    }
+    async consultarIngrediente(req, res) {
+        try {
+            const ingredientes = await Ingrediente.findAll();
+            res.status(200).json(ingredientes);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Erro ao consultar ingredientes' });
+        }
+    }
+    async consultarProdutoIngrediente(req, res) {
+        try {
+            const produtosIngredientes = await ProdutoIngrediente.findAll({});
+            res.status(200).json(produtosIngredientes);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Erro ao consultar produtos ingredientes' });
+        }
+    }
+    async consultarFuncionario(req, res) {
+        try {
+            const funcionarios = await Funcionario.findAll();
+            res.status(200).json(funcionarios);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Erro ao consultar funcionários' });
+        }
+    }
+    async consultarCliente(req, res) {
+        try {
+            const clientes = await Cliente.findAll();
+            res.status(200).json(clientes);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Erro ao consultar clientes' });
+        }
+    }
+    async consultarVenda(req, res) {
+        try {
+            const vendas = await Venda.findAll({
+                include: [
+                    {
+                        model: Funcionario,
+                        attributes: ['nome']
+                    },
+                    {
+                        model: Cliente,
+                        attributes: ['nome']
+                    }
+                ]
+            });
+            res.status(200).json(vendas);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Erro ao consultar vendas' });
+        }
+    }
+}
+
+module.exports = new ConsultaController();
---
+++ server/src/routes/consultaRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const consultaController = require('../controllers/consultaController');
+
+router.get('/consulta/produtos', consultaController.consultarProduto);
+
+router.get('/consulta/ingredientes', consultaController.consultarIngrediente);
+
+router.get('/consulta/produtoingrediente', consultaController.consultarProdutoIngrediente);
+
+router.get('/consulta/funcionarios', consultaController.consultarFuncionario);
+
+router.get('/consulta/clientes', consultaController.consultarCliente);
+
+router.get('/consulta/vendas', consultaController.consultarVenda);
+
+module.exports = router;
